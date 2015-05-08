cd6585b5509697eb17a5b20d01b5f9fd30ccc4fa
bujjibabu <bujji404@gmail.com>
2015-05-08 07:50:17 +0530

    js session..

---
+++ Bujji/js/js1.js
@@ -0,0 +1,32 @@
+//var x = 10;
+		/*var X = 'ram';
+		var y = 12.34;
+		var z = 'bujji';
+		var a = true;
+		var myobj = {name:'bujji',role:'lead'};
+		var myarr = [];*/
+		// numbers 
+		// ex: 12, 23.45 etc...
+		// string 
+		// ex: 'bujji',"babu" etc...
+		// boolean
+		// true, false;
+
+		var x = 20;
+		var y = 30;
+		var z = x+y;
+		document.write(z);
+		var mydate = new Date();
+
+		console.log(mydate);
+		//alert(mydate);
+
+		//comparison
+		var x = 10;
+		var y = '10';
+		 console.log(x==y);
+		 if(x===y){
+		 	alert('both are equel')
+		 }else{
+		 	alert('not equel')
+		 }
