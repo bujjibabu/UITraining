0d1ea85627a6be2a4c29f45052fc25f978fcc5db
venkat <venkatreddy2662@gmail.com>
2015-05-12 17:35:11 -0500

    External Javascript

---
+++ Venkat/js/venkat.js
@@ -0,0 +1,12 @@
+var name = "VenkatReddy"
+alert(name.length)
+
+var myArray1 = [1,6,5,4];
+var myArray2 = ['Naveen','Pramod','Pavan','Akshitha'];
+var myArray3 = [1,'Sree',0.00,'Chinna'];
+document.write(myArray1[2])
+document.write(myArray2[2])
+document.write(myArray3[2])
+alert(myArray2[3]);
+
+
